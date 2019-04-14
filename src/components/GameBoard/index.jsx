32d92bda6c7c19b3845a507a93ed29e794531877
vince19972 <vince19972@gmail.com>
2019-04-14 15:54:57 -0400

    :lipstick: add video and game title

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
-import './GameBoard.css'
+import React, { useEffect } from 'react'
+import './GameBoard.scss'
 
 import SnakeCanvas from '../SnakeCanvas'
+import loadVideo from '../../utilities/camera'
 
 function GameBoard () {
+
+	useEffect(() => {
+		loadVideo(document.getElementById('userWebCam'))
+	})
+
 	return (
 		<div id="GameBoard">
-			<SnakeCanvas/>
+			<div className="info-bar">
+				<h1 className="game-title">Teachable Snake</h1>
+				<video id="userWebCam"></video>
+			</div>
+			<div className="main-canvas">
+				<SnakeCanvas/>
+			</div>
 		</div>
 	)
 }
