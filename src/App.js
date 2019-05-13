74f3ef4847468fb4cc91842982400585baaf38e3
vince19972 <vince19972@gmail.com>
2019-05-13 10:15:46 -0400

    [App.js] remove unused vars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { StoreContext } from './context/StoreContext'
 import './App.css'
 import './styles/_variables.css'
